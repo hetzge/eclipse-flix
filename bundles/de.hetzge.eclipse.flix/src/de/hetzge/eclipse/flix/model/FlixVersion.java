9c174d9d006cdaf8615d0748941f6e59921c898d
hetzge <hetzge@gmx.de>
2024-05-09 22:09:48 +0200

    Add new flix versions

--- bundles/de.hetzge.eclipse.flix/src/de/hetzge/eclipse/flix/model/FlixVersion.java
+++ bundles/de.hetzge.eclipse.flix/src/de/hetzge/eclipse/flix/model/FlixVersion.java
@@ -12,8 +12,21 @@ public final class FlixVersion {
 	public final static FlixVersion VERSION_0_34_0 = new FlixVersion("0.34.0");
 	public final static FlixVersion VERSION_0_42_0 = new FlixVersion("0.42.0");
 	public final static FlixVersion VERSION_0_43_0 = new FlixVersion("0.43.0");
+	public final static FlixVersion VERSION_0_44_0 = new FlixVersion("0.44.0");
+	public final static FlixVersion VERSION_0_45_0 = new FlixVersion("0.45.0");
+	public final static FlixVersion VERSION_0_46_0 = new FlixVersion("0.46.0");
 	public final static FlixVersion CUSTOM = new FlixVersion("<project>/flix.jar");
-	public final static List<FlixVersion> VERSIONS = List.of(VERSION_0_30_0, VERSION_0_31_0, VERSION_0_32_0, VERSION_0_33_0, VERSION_0_34_0, VERSION_0_42_0, VERSION_0_43_0);
+	public final static List<FlixVersion> VERSIONS = List.of(
+			VERSION_0_30_0,
+			VERSION_0_31_0,
+			VERSION_0_32_0,
+			VERSION_0_33_0,
+			VERSION_0_34_0,
+			VERSION_0_42_0,
+			VERSION_0_43_0,
+			VERSION_0_44_0,
+			VERSION_0_45_0,
+			VERSION_0_46_0);
 
 	private final String key;
 
